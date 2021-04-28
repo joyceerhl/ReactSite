b09ad82b3372afbb354e18ef44693cf123541c4f
nturinski <naturins@microsoft.com>
2021-04-28 14:04:57 -0700

    Test

--- app/src/index.js
+++ app/src/index.js
@@ -10,22 +10,22 @@ import axios from 'axios';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 class FetchDemo extends React.Component {
-    state = {
-      userName: ''
-    }
-  
-    componentDidMount() {
-      axios.get(`/api/${this.props.subreddit}`)
-        .then(res => {
-          console.log(res.data);
-          var userName = 'Hey ' + res.data.userDetails + '!';
-          this.setState({userName});
-        });
-    }
-  
-    render() {
-      return (
-        <div className="App">
+  state = {
+    userName: ''
+  }
+
+  componentDidMount() {
+    axios.get(`/api/${this.props.subreddit}`)
+      .then(res => {
+        console.log(res.data);
+        var userName = 'Hey there' + res.data.userDetails + '!';
+        this.setState({ userName });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -36,14 +36,14 @@ class FetchDemo extends React.Component {
           <p>{this.state.userName}</p>
         </header>
       </div>
-      );
-    }
+    );
   }
-  
-  ReactDOM.render(
-    <FetchDemo subreddit="getList"/>,
-    document.getElementById('root')
-  );
+}
+
+ReactDOM.render(
+  <FetchDemo subreddit="getList" />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
